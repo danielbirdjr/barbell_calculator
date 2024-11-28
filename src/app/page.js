c0692e0b6736e93a48c60a3d10318f4a1ec77ef2
Daniel Bird <danielbirdpersonal@gmail.com>
2024-11-27 20:11:30 -0500

    Added basic html structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      <div className="nav-bar">
+        <h1>Barbell Calculator</h1>
+      </div>
+      <div className="barbell-display-container">
+        <div className="weight-display"></div>
+        <div className="barbell-display"></div>
+      </div>
+      <div className="calculation-and-options-container">
+        <div className="enter-weight-and-input-container">
+          <h3>Enter Weight</h3>
+          <input></input>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="options-container">
+          <div className="units-container">
+            <h3>Select Units</h3>
+            <div className="units-options-container">
+              <div className="weight-unit">
+                <input type="checkbox"></input>
+                <label>LB</label>
+              </div>
+              <div className="weight-unit">
+                <input type="checkbox"></input>
+                <label>KG</label>
+              </div>
+              
+            </div>
+          </div>
+          <div className="barbell-weight-container">
+            <h3>Select Barbell</h3>
+            <div className="barbell-weight-options">
+              <button>45 lb</button>
+              <button>20 lb</button>
+              <button>0 lb</button>
+            </div>
+          </div>
+        </div>
+        <div className="calculate-button-container">
+          <button>Calculate plates</button>
+        </div>
+      </div>
+    </main>
   );
 }
