685a049d2cd684f53390db06051e4fcb47f220a9
Daniel Bird <danielbirdpersonal@gmail.com>
2024-12-19 20:36:45 -0500

    Restructed folders and added file for rep max calculator

---
+++ src/app/rm-calculator/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import '../styles/globals.css'
+import './rm-calculator.css'; 
+
+export default function RepMaxCalculator() {
+
+  return (
+    <main>
+      <div>Hello World</div>
+    </main>
+  );
+}
