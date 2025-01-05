00c6f0d186659a366ac46cf0b0f12df50ce52aac
Daniel Bird <danielbirdpersonal@gmail.com>
2025-01-05 12:24:39 -0500

    Got the navbar completed for now

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from "next/link";
 import './NavBar.css'; 
 import { FiMenu } from "react-icons/fi";
@@ -17,30 +17,66 @@ const NavBar = () => {
         setIsDropdownVisible(false);
     }
 
+    // Collapse menu when clicking outside
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            const dropdownMenu = document.querySelector(".dropdown-menu");
+            const menuIcon = document.querySelector(".menu-icon");
+
+            if (
+                dropdownMenu &&
+                !dropdownMenu.contains(event.target) &&
+                !menuIcon.contains(event.target)
+            ) {
+                closeDropdown();
+            }
+        };
+
+        document.addEventListener("click", handleOutsideClick);
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
+
+    // Automatically close the dropdown when navigating
+    useEffect(() => {
+        const navLinks = document.querySelectorAll(".dropdown-menu a");
+
+        const handleLinkClick = () => {
+            closeDropdown();
+        };
+
+        navLinks.forEach((link) =>
+            link.addEventListener("click", handleLinkClick)
+        );
+
+        return () => {
+            navLinks.forEach((link) =>
+                link.removeEventListener("click", handleLinkClick)
+            );
+        };
+    }, []);
+
+
     return (
         <nav className="navbar">
 
             <div className="navbar-content">
                 <Link href="/" className="navbar-logo">The Name</Link>
-                <button className={`menu-icon ${isDropdownVisible ? "hidden" : ""}`} onClick={toggleDropdown}>
-                    <FiMenu />
-                </button>
-                <button className={`menu-icon close-menu ${isDropdownVisible ? "" : "hidden"}`} onClick={closeDropdown} >
-                    <FiX />
-                </button>
+                <button className={`menu-icon ${isDropdownVisible ? "hidden" : ""}`} onClick={toggleDropdown}><FiMenu /></button>
+                <button className={`menu-icon close-menu ${isDropdownVisible ? "" : "hidden"}`} onClick={closeDropdown} ><FiX /></button>
             </div>
             <ul className={`dropdown-menu ${isDropdownVisible ? "visible" : ""}`}>
                 <li><Link href="/barbell-calculator">Barbell Calculator</Link></li>
-                <li><Link href="/rm-calculator">1 Rep Max Calculator</Link></li>
+                <li><Link href="/rm-calculator">1 RM Calculator</Link></li>
                 <li><Link href="/rm-calculator">Reps by RM Calculator</Link></li>
                 <li><Link href="https://progressai.vercel.app/" target='_blank'>Fitness App</Link></li>
             </ul>
 
             <ul className="nav-links">
                 <li><Link href="/barbell-calculator">Barbell Calculator</Link></li>
-                <li><Link href="/rm-calculator">1 Rep Max Calculator</Link></li>
-                <li><Link href="/rm-calculator">Reps by RM Calculator</Link></li>
-                <li><Link href="https://progressai.vercel.app/" target='_blank'>Fitness App</Link></li>
+                <li><Link href="/rm-calculator">RM Calculators</Link></li>
+                <li className='fitness-app'><Link href="https://progressai.vercel.app/" target='_blank'>Fitness App</Link></li>
             </ul>
 
         </nav>
