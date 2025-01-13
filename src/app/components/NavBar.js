9c6423523a0e225fec558a5c77844998007ab833
Daniel Bird <danielbirdpersonal@gmail.com>
2025-01-13 16:19:17 -0500

    Added popup menu for different calculators on desktop view. Also think i removed the background color from the barbell calculator title

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import './NavBar.css'; 
 import { FiMenu } from "react-icons/fi";
 import { FiX } from "react-icons/fi";
+import { FiChevronDown } from "react-icons/fi";
 
 const NavBar = () => {
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -57,6 +58,24 @@ const NavBar = () => {
         };
     }, []);
 
+    // RM Calculators pop up menu
+    const [isRMCalculatorPopUpMenuVisible, setIsRMCalculatorPopUpMenuVisible] = useState(false);
+    const togglePopupMenu = () => {
+        setIsRMCalculatorPopUpMenuVisible(prevState => !prevState);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+          if (!event.target.closest('.rm-calculators')) {
+            setIsRMCalculatorPopUpMenuVisible(false);
+          }
+        };
+      
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+          document.removeEventListener('click', handleClickOutside);
+        };
+      }, []);
 
     return (
         <nav className="navbar">
@@ -75,10 +94,16 @@ const NavBar = () => {
 
             <ul className="nav-links">
                 <li><Link href="/barbell-calculator">Barbell Calculator</Link></li>
-                <li><Link href="/rm-calculator">RM Calculators</Link></li>
+                <li className='rm-calculators' onClick={togglePopupMenu}>RM Calculators <FiChevronDown />
+                    {isRMCalculatorPopUpMenuVisible && (
+                        <ul className='rm-calculators-popup-menu'>
+                            <li><Link href="/rm-calculator?calculator=1RM">1 RM Calculator</Link></li>
+                            <li><Link href="/rm-calculator?calculator=Weight-for-Reps">Weight for Reps Calculator</Link></li>
+                        </ul>
+                    )}
+                </li>
                 <li className='fitness-app'><Link href="https://progressai.vercel.app/" target='_blank'>Fitness App</Link></li>
             </ul>
-
         </nav>
     );
 };
